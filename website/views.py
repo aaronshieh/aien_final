1e47e50a3eedf62758862501a350cf514bb42d6b
Aaron <aien0322@gmail.com>
2018-12-13 13:13:28 +0800

    add detect_intent_audio

--- website/views.py
+++ website/views.py
@@ -5,8 +5,10 @@ import dialogflow, json, requests
 from google.protobuf.json_format import MessageToJson
 
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="TaipeiBus-d9a21c23d606.json"
-DIALOGFLOW_PROJECT_ID = 'taipeibus-3f5d4'
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="TaipeiBus-d9a21c23d606.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="test-6853bf0aa060.json"
+# DIALOGFLOW_PROJECT_ID = 'taipeibus-3f5d4'
+DIALOGFLOW_PROJECT_ID = 'test-7405b'
 
 def index(request):
     title = 'main'
@@ -87,7 +89,7 @@ def detect_intent_texts(project_id, session_id, text, language_code):
         text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
         query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
-        print(response.query_result)
+        # print(response.query_result)
         return response.query_result.fulfillment_messages
 
 @csrf_exempt
@@ -95,8 +97,10 @@ def send_message(request):
     if request.method == 'POST':
         message = request.POST['message']
         project_id = DIALOGFLOW_PROJECT_ID
-        fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
-        print(type(fulfillment_text))
+        # fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+        fulfillment_text = detect_intent_audio(project_id, "unique", 'hello.wav', 'en')
+        # print(type(fulfillment_text))
+        print("audio test", fulfillment_text)
         fulfillment_obj = []
         for obj in fulfillment_text:
             fulfillment_obj.append(json.loads(MessageToJson(obj)))
@@ -105,3 +109,38 @@ def send_message(request):
         response_text = { "message":  fulfillment_obj }
         print(response_text)
         return JsonResponse(response_text)
+
+def detect_intent_audio(project_id, session_id, audio_file_path,
+                        language_code):
+    """Returns the result of detect intent with an audio file as input.
+    Using the same `session_id` between requests allows continuation
+    of the conversaion."""
+    # import dialogflow_v2 as dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    # Note: hard coding audio_encoding and sample_rate_hertz for simplicity.
+    audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
+    sample_rate_hertz = 16000
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    with open(audio_file_path, 'rb') as audio_file:
+        input_audio = audio_file.read()
+
+    audio_config = dialogflow.types.InputAudioConfig(
+        audio_encoding=audio_encoding, language_code=language_code)
+    query_input = dialogflow.types.QueryInput(audio_config=audio_config)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input,
+        input_audio=input_audio)
+
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
